2ba08d526c4b24f02442058c4f3a63f6404553af
yeputons <egor.suvorov@gmail.com>
2014-02-19 15:34:15 +0400

    smart-publish.js: field counters and per-cursor storages were added, all tests pass now

--- smart-publish.js
+++ smart-publish.js
@@ -7,7 +7,18 @@ Meteor.smartPublish = function(name, callback) {
     var self = this;
     var collections = {};
 
-    self.smartAdded = function(name, id, fields) {
+    updateFromData = function(name, id, fields) {
+      var res = {};
+      _.each(fields, function(flag, key) {
+        var cur = undefined;
+        _.each(collections[name][id].data[key], (function(value) {
+          cur = _.extend(value, cur);
+        }));
+        res[key] = cur;
+      });
+      self.changed(name, id, res);
+    }
+    smartAdded = function(name, index, id, fields) {
       if (!name)
         throw new Meteor.Error("Trying to add element to anonymous collection");
 
@@ -15,13 +26,31 @@ Meteor.smartPublish = function(name, callback) {
 
       if (!collections[name][id]) {
         self.added(name, id, fields);
-        collections[name][id] = { count: 1 };
+        collections[name][id] = { count: 1, data: {} };
+        _.each(fields, function(val, key) {
+          collections[name][id].data[key] = collections[name][id].data[key] || {};
+          collections[name][id].data[key][index] = val;
+        });
       } else {
-        self.changed(name, id, fields);
+        _.each(fields, function(val, key) {
+          collections[name][id].data[key] = collections[name][id].data[key] || {};
+          collections[name][id].data[key][index] = val;
+        });
         collections[name][id].count++;
+        updateFromData(name, id, fields);
       }
     }
-    self.smartRemoved = function(name, id) {
+    smartChanged = function(name, index, id, fields) {
+      _.each(fields, function(val, key) {
+        if (val === undefined) {
+          delete collections[name][id].data[key][index];
+        } else {
+          collections[name][id].data[key][index] = val;
+        }
+      });
+      updateFromData(name, id, fields);
+    }
+    smartRemoved = function(name, index, id) {
       if (!name                 ) throw new Meteor.Error("Trying to remove element from anonymous collection");
       if (!collections[name]    ) throw new Meteor.Error("Removing element from non-existing collection '" + name + "'");
       if (!collections[name][id]) throw new Meteor.Error("Removing unexisting element '" + id + "' from collection '" + name + "'");
@@ -29,6 +58,15 @@ Meteor.smartPublish = function(name, callback) {
       if (!--collections[name][id].count) { // If reference counter was decremented to zero
         delete collections[name][id];
         self.removed(name, id);
+      } else {
+        var fields = {};
+        _.each(collections[name][id].data, function(vals, key) {
+          if (index in vals) {
+            fields[key] = 1;
+            delete vals[index];
+          }
+        });
+        updateFromData(name, id, fields);
       }
     }
     
@@ -49,17 +87,17 @@ Meteor.smartPublish = function(name, callback) {
 
       if (!c._cursorDescription) throw new Meteor.Error("Unable to get cursor's collection name");
 
-      function publishCursor(name) {
+      function publishCursor(name, index) {
         observers.push(c.observeChanges({
-          added:   function(id, fields) {self.smartAdded  (name, id, fields); },
-          changed: function(id, fields) {self.changed     (name, id, fields); },
-          removed: function(id)         {self.smartRemoved(name, id);         },
+          added:   function(id, fields) {smartAdded  (name, index, id, fields); },
+          changed: function(id, fields) {smartChanged(name, index, id, fields); },
+          removed: function(id)         {smartRemoved(name, index, id);         },
         }));
       };
 
       var name = c._cursorDescription.collectionName;
       if (!name) throw new Meteor.Error("Unable to get cursor's collection name");
-      publishCursor(name); // closure is require for the 'name' variable
+      publishCursor(name, i);
     }
     this.ready();
     this.onStop(function() {
