ced797a29f456ebe43f5fd42ca10cda66ef03d9e
yeputons <egor.suvorov@gmail.com>
2014-02-19 14:31:59 +0400

    Added more tests with top-level fields filtering

--- tests/several-cursors.js
+++ tests/several-cursors.js
@@ -5,16 +5,16 @@ if (Meteor.isServer) {
   ItemsA.remove({});
   ItemsB.remove({});
   for (var i = 1; i <= 10; i++) {
-    ItemsA.insert({val: i});
-    ItemsB.insert({val: i});
+    ItemsA.insert({val: i, a: 1, b: 1});
+    ItemsB.insert({val: i, a: 1, b: 1});
   }
 
   Meteor.smartPublish('items', function(l, r) {
     return [
-      ItemsA.find({val: {$lt: l}}),
-      ItemsA.find({val: {$gt: r}}),
-      ItemsB.find({val: {$lt: r}}),
-      ItemsB.find({val: {$gt: l}}),
+      ItemsA.find({val: {$lt: l}}, {fields: {val: 1, a: 1}}),
+      ItemsA.find({val: {$gt: r}}, {fields: {val: 1, b: 1}}),
+      ItemsB.find({val: {$lt: r}}, {fields: {val: 1, a: 1}}),
+      ItemsB.find({val: {$gt: l}}, {fields: {val: 1, b: 1}}),
     ];
   });
   Meteor.methods({
@@ -31,8 +31,8 @@ if (Meteor.isClient) {
     next();
   });
 
-  function getVals(coll) {
-    return _.pluck(coll.find({}, {fields: {val: 1}}).fetch(), 'val')
+  function getVals(coll, filter) {
+    return _.pluck(coll.find(filter || {}, {fields: {val: 1}}).fetch(), 'val')
   }
 
   var subscr;
@@ -40,6 +40,11 @@ if (Meteor.isClient) {
     subscr = Meteor.subscribe('items', 4, 9, function() {
       test.equal(getVals(ItemsA), [1,2,3,10], 'ItemsA is invalid');
       test.equal(getVals(ItemsB), [1,2,3,4,5,6,7,8,9,10], 'ItemsB is invalid');
+
+      test.equal(getVals(ItemsA, {a: 1}), [1,2,3], 'ItemsA.a is invalid');
+      test.equal(getVals(ItemsA, {b: 1}), [10], 'ItemsA.b is invalid');
+      test.equal(getVals(ItemsB, {a: 1}), [1,2,3,4,5,6,7,8], 'ItemsB.a is invalid');
+      test.equal(getVals(ItemsB, {b: 1}), [5,6,7,8,9,10], 'ItemsB.b is invalid');
       next();
     });
   });
@@ -56,6 +61,45 @@ if (Meteor.isClient) {
     Meteor.call('updateA6To0', function(err, res) {
       test.isUndefined(err, 'error during method call: ' + err);
       test.equal(getVals(ItemsA), [1,3,10,0], 'ItemsA');
+      next();
+    });
+  });
+
+  Tinytest.addAsync('update with field add', function(test, next) {
+    var id = ItemsB.findOne({val: 2})._id;
+    test.equal(ItemsB.findOne(id), {_id: id, val: 2, a: 1}, 'initial fieldset');
+    ItemsB.update(id, {$set: {val: 5}}, function(err, res) {
+      test.isUndefined(err, 'error during update: ' + err);
+      test.equal(ItemsB.findOne(id), {_id: id, val: 5, a: 1, b: 1}, 'resulting fieldset');
+      next();
+    });
+  });
+
+  Tinytest.addAsync('update with field remove', function(test, next) {
+    var id = ItemsB.findOne({val: 5})._id;
+    test.equal(ItemsB.findOne(id), {_id: id, val: 5, a: 1, b: 1}, 'initial fieldset');
+    ItemsB.update(id, {$set: {val: 0}}, function(err, res) {
+      test.isUndefined(err, 'error during update: ' + err);
+      test.equal(ItemsB.findOne(id), {_id: id, val: 0, a: 1}, 'resulting fieldset');
+      next();
+    });
+  });
+
+  Tinytest.addAsync('update with field remove&add', function(test, next) {
+    var id = ItemsB.findOne({val: 3})._id;
+    test.equal(ItemsB.findOne(id), {_id: id, val: 3, a: 1}, 'initial fieldset');
+    ItemsB.update(id, {$set: {val: 12}}, function(err, res) {
+      test.isUndefined(err, 'error during update: ' + err);
+      test.equal(ItemsB.findOne(id), {_id: id, val: 12, b: 1}, 'resulting fieldset');
+      next();
+    });
+  });
+  Tinytest.addAsync('update with field add&remove', function(test, next) {
+    var id = ItemsB.findOne({val: 10})._id;
+    test.equal(ItemsB.findOne(id), {_id: id, val: 10, b: 1}, 'initial fieldset');
+    ItemsB.update(id, {$set: {val: 3}}, function(err, res) {
+      test.isUndefined(err, 'error during update: ' + err);
+      test.equal(ItemsB.findOne(id), {_id: id, val: 3, a: 1}, 'resulting fieldset');
       next();
     });
   });
